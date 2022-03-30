efd46d5c1839b711cff02ec2f7c89b982a9b121c
Crozas17 <carlosrozasrodriguez2001@gmail.com>
2022-03-30 19:35:02 +0200

    Cambios tarde miercoles
    Cambios en expo

--- js/exposicion.js
+++ js/exposicion.js
@@ -1,8 +1,22 @@
 $(".expo").click(function(){
+    $("#destacada_img").html("");
+    $("#destacada_texto").html("");
     var img=$(this).attr("id");
     var nombre=$(this).attr("alt");
-    $("#destacada").html("<img id=\"img_destacada\" src=\"../images/expo_items/"+img+"/"+img+".jpeg\" alt=\""+nombre+" ><\" />"+"<h1> "+nombre+"</h1>");
-    //$("#destacada").html("<h1> "+nombre+"</h1>");
-    //$("#destacada").html("<h1> <img id=\"img_destacada\" src=\"../images/expo_items/"+img+"/"+img+".jpeg\" ></img> "+nombre+"</h1>");
-    $('html, body').animate({scrollTop: 0}, '300');
+    $('#ventana').hide();
+    $('.row').css('opacity','0.3');
+
+    
+
+     $('#ventana').show( "slow" ,function(){
+    //     $("#destacada_texto").html("<img id=\"img_destacada\" src=\"../images/expo_items/"+img+"/"+img+".jpeg\" alt=\""+nombre+" ><\" />"+"<h1>"+nombre+"</h1>");
+        $("#destacada_img").html("<img id=\"img_destacada\" src=\"../images/expo_items/"+img+"/"+img+".jpeg\" alt=\""+nombre+"\">");
+        $("#destacada_texto").html("<h1>"+nombre+"</h1>");
+    });
+});
+
+
+$("#cruz").click(function(){
+    $('#ventana').hide("slow");
+    $('.row').css('opacity','1');
 });
