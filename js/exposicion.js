f86d669b60388f75c5bd64b29f8ec3337d92467c
Crozas17 <carlosrozasrodriguez2001@gmail.com>
2022-03-30 23:35:22 +0200

    XML tazas
    Se añade xml para la info de las tazas.

--- js/exposicion.js
+++ js/exposicion.js
@@ -1,15 +1,35 @@
+var color=[];
+var autor=[];
+
+$(document).ready(function(){
+    $.ajax({
+        url:'../xml/tazas.xml',
+        dataType:'xml',
+        success: function(data){
+            $(data).find('TAZAS taza').each(function(){
+                var col=$(this).find('color').text();
+                var aut=$(this).find('autor').text();
+                color.push(col);
+                autor.push(aut);
+            });
+        },
+        error: function(){
+            console.log();
+        }
+    });
+});
+
 $(".expo").click(function(){
     $("#destacada_img").html("");
     $("#destacada_texto").html("");
+    $("#destacada_desc").html("");
     var img=$(this).attr("id");
     var nombre=$(this).attr("alt");
     $('#ventana').hide();
     $('.row').css('opacity','0.3');
 
-    
-
      $('#ventana').show( "slow" ,function(){
-    //     $("#destacada_texto").html("<img id=\"img_destacada\" src=\"../images/expo_items/"+img+"/"+img+".jpeg\" alt=\""+nombre+" ><\" />"+"<h1>"+nombre+"</h1>");
+        $("#destacada_desc").html("<p>Autor: "+autor[img.split('_')[1]]+".</p>"+"<p>Color: "+color[img.split('_')[1]]+".</p>");
         $("#destacada_img").html("<img id=\"img_destacada\" src=\"../images/expo_items/"+img+"/"+img+".jpeg\" alt=\""+nombre+"\">");
         $("#destacada_texto").html("<h1>"+nombre+"</h1>");
     });
